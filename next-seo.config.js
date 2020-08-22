d832cbb42aa298b069b29552a2d850f3c2762100
CAM603 <cameronhawley@comcast.net>
2020-08-22 16:14:33 -0400

    add default seo

---
+++ next-seo.config.js
@@ -0,0 +1,23 @@
+export default {
+    title: "Hawley Communications",
+    description: "Hawley Communications.",
+    openGraph: {
+        type: "website",
+        locale: "en_IE",
+        url: "",
+        site_name: "Hawley Communications",
+        images: [
+            {
+                url: "",
+                width: 32,
+                height: 32,
+                alt: "Hawley Communications",
+            },
+        ],
+    },
+    twitter: {
+        handle: "@none",
+        site: "@none",
+        cardType: "summary_large_image",
+    },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 // Styles
 import GlobalStyles from "../components/GlobalStyles/GlobalStyles";
+import { ThemeProvider } from "emotion-theming";
+import theme from "../theme/theme";
+// SEO
+import { DefaultSeo } from "next-seo";
+import SEO from "../next-seo.config";
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
-            <GlobalStyles />
-            <Component {...pageProps} />
+            <DefaultSeo {...SEO} />
+            <ThemeProvider theme={theme}>
+                <GlobalStyles />
+                <Component {...pageProps} />
+            </ThemeProvider>
         </>
     );
 }

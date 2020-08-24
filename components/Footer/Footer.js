2d0b7e038eecfd662bb21b0d3d11dd3bc89976c0
CAM603 <cameronhawley@comcast.net>
2020-08-24 19:34:54 -0400

    add boilerplate footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,7 +1,31 @@
+// Styles
+import styled from "@emotion/styled";
+import NavigationItems from "../Navigation/NavigationItems/NavigationItems";
+
 export default function Footer(props) {
     return (
-        <footer>
-            <p>Footer</p>
-        </footer>
+        <FooterStyled>
+            <div>
+                <p>Logo</p>
+            </div>
+            <nav>
+                <NavigationItems />
+            </nav>
+            <div>
+                <p>Email</p>
+                <p>LinkedIn</p>
+            </div>
+        </FooterStyled>
     );
 }
+
+const FooterStyled = styled.footer`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    background-color: black;
+    color: white;
+    height: 100px;
+    padding: 20px;
+`;
